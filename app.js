5c84f724c4085b68da1d1c67ec3b22607a839c85
Carlos Plata <carlos.plata282@gmail.com>
2024-11-11 18:55:45 -0600

    added paginator to display code for playlist that have more than 50 videos

--- app.js
+++ app.js
@@ -4,38 +4,84 @@ const fetchButton = document.getElementById("submit-link-button");
 const copyButton = document.getElementById("copy-clipboard-button");
 const statusText = document.getElementById("fetch-status");
 
+const paginationContainer = document.getElementsByClassName("pagination-container")[0];
+const [previousPageButton, nextPageButton] = document.getElementsByClassName("page-bttn");
+
 fetchButton.addEventListener("click", getPlaylistInformation);
+previousPageButton.addEventListener("click", getPreviousPageInformation);
+nextPageButton.addEventListener("click", getNextPageInformation);
 copyButton.addEventListener("click", copyToClipboard);
 
+let currentPlaylist;
+let previousPageId = null;
+let nextPageId = null;
+let morePages = false;
+
+togglePaginator();
+
 async function getPlaylistInformation() {
+  iframesOutput.textContent = ""
   const url = listInput.value.trim();
   const regex = /(?:playlist\?list=|\/playlist\?list=|(?:\?|\&)list=)([a-zA-Z0-9_-]{16,})/;
 
   const playlistID = url.match(regex);
-  
-  if (playlistID == null){
+
+  if (playlistID == null) {
     statusText.textContent = "The input is not a valid Youtube Playlist link or ID";
     return
   }
   statusText.textContent = "Loading...";
-  const playlist = await fetchPlaylist(playlistID[1]);
+  const playlist = await fetchPlaylist(playlistID[1], "");
 
-  if (playlist.status.http_code == 404){
+  if (playlist.status.http_code == 404) {
     statusText.textContent = "Playlist not found! Verify the URL is correct";
     console.error(playlist.contents);
     return
   }
 
-  if (playlist.status.http_code == 403){
+  if (playlist.status.http_code == 403) {
     statusText.textContent = "Playlist forbidden! You can't fetch from the Watch later or History playlists";
     console.error(playlist.contents);
     return
   }
 
-  startFormatting(JSON.parse(playlist.contents));
+  currentPlaylist = playlistID[1];
+  const playlistData = JSON.parse(playlist.contents);
+  previousPageId = playlistData.prevPageToken;
+  nextPageId = playlistData.nextPageToken;
+
+  console.log(playlistData);
+
+  togglePaginator();
+  startFormatting(playlistData);
 }
 
-function startFormatting(videoData){
+async function getNextPageInformation() {
+  iframesOutput.textContent = ""
+  statusText.textContent = "Loading...";
+  const playlist = await fetchPlaylist(currentPlaylist, nextPageId);
+  const playlistData = JSON.parse(playlist.contents);
+  previousPageId = playlistData.prevPageToken;
+  nextPageId = playlistData.nextPageToken;
+
+  togglePaginator();
+  startFormatting(playlistData);
+}
+
+async function getPreviousPageInformation() {
+  iframesOutput.textContent = ""
+  statusText.textContent = "Loading...";
+  const playlist = await fetchPlaylist(currentPlaylist, previousPageId);
+  const playlistData = JSON.parse(playlist.contents);
+  previousPageId = playlistData.prevPageToken;
+  nextPageId = playlistData.nextPageToken;
+
+  togglePaginator();
+  startFormatting(playlistData);
+}
+
+function startFormatting(videoData) {
+  console.log(videoData);
   iframesOutput.textContent = ""
   videoData.items.forEach(video => {
     if (video.status.privacyStatus != 'public') return
@@ -44,9 +90,15 @@ function startFormatting(videoData){
   statusText.textContent = "Here's your code!";
 }
 
-async function fetchPlaylist(playlistID) {
-  const apiKey = 'AIzaSyA7lJQiUbPBizcZA4nd7yYlcVFnPAuTbrA'; 
-  const url = `https://api.allorigins.win/get?url=${encodeURIComponent(`https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails,status&maxResults=50&playlistId=${playlistID}&key=${apiKey}`)}`;
+async function fetchPlaylist(playlistID, pageToken) {
+  const apiKey = 'AIzaSyA7lJQiUbPBizcZA4nd7yYlcVFnPAuTbrA';
+  let url = "";
+
+  if (pageToken == "") {
+    url = `https://api.allorigins.win/get?url=${encodeURIComponent(`https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails,status&maxResults=50&playlistId=${playlistID}&key=${apiKey}`)}`;
+  } else {
+    url = `https://api.allorigins.win/get?url=${encodeURIComponent(`https://www.googleapis.com/youtube/v3/playlistItems?part=contentDetails,status&maxResults=50&playlistId=${playlistID}&pageToken=${pageToken}&key=${apiKey}`)}`;
+  }
 
   try {
     const response = await fetch(url);
@@ -72,4 +124,19 @@ function copyToClipboard() {
     copyButton.textContent = "Successfully copied!";
   }
   setTimeout(() => { copyButton.textContent = "Copy to Clipboard" }, 2000)
+}
+
+function togglePaginator() {
+  if (previousPageId != null) {
+    previousPageButton.style.display = "block"
+  } else {
+    previousPageButton.style.display = "none";
+  };
+
+  if (nextPageId != null) {
+    nextPageButton.style.display = "block"
+  } else {
+    nextPageButton.style.display = "none";
+  };
+
 }
